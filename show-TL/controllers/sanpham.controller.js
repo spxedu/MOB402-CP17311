17b188953b3e3414fe1f335667b7593c6461a502
spxedu <62041731+spxedu@users.noreply.github.com>
2023-04-01 14:04:38 +0700

    Làm chức năng sửa

--- show-TL/controllers/sanpham.controller.js
+++ show-TL/controllers/sanpham.controller.js
@@ -42,3 +42,45 @@ exports.add = async (req, res, next)=>{
     }
     res.render('sanpham/add',{ msg: msg, listTL: listTL  })
 }
+
+
+exports.editSP = async (req,res,next)=>{
+    // tạo biến lưu câu thông báo
+    let msg = ''; // dùng truyền ra view
+    
+    //load ds thể loại đưa lên giao diện
+    let listTL = await myMD.theLoaiMD.find();
+    console.log(listTL);
+    // load thông tin sản phẩm
+    let idsp = req.params.idsp;
+    let objSP = await myMD.spModel.findById(idsp);
+
+
+    if(req.method =='POST'){
+        // kiểm tra hợp lệ dữ liệu
+
+        // tạo đối tượng model để gán dữ liệu
+        let objSP = new myMD.spModel();
+        objSP.name = req.body.name; 
+        objSP.price = req.body.price;
+        objSP.id_theloai = req.body.theloai; // thêm ID thể loại
+
+        objSP._id = idsp; // thêm cho sửa
+
+        // ghi CSDL
+        try {
+            // let new_sp = await objSP.save();
+            // console.log(new_sp);
+            // msg = 'Đã thêm thành công';
+            await myMD.spModel.findByIdAndUpdate({_id: idsp}, objSP );
+            msg = 'Đã sửa thành công';
+
+        } catch (error) {
+            msg = 'Lỗi '+ error.message;
+            console.log(error);
+        }
+    }
+
+
+    res.render('sanpham/edit', { msg: msg, listTL: listTL, objSP:objSP});
+}
--- show-TL/routes/sanpham.js
+++ show-TL/routes/sanpham.js
@@ -7,4 +7,8 @@ router.get('/', spCtrl.list);
 router.get('/add', spCtrl.add);
 router.post('/add', spCtrl.add);
 
+router.get('/edit/:idsp', spCtrl.editSP);
+router.post('/edit/:idsp', spCtrl.editSP);
+
+
 module.exports = router;
